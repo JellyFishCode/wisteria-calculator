6c3ccfe216e6ec14516905e5c18ebdbacff0d5dd
Sammi Vay <sammi.vay@visol.ch>
2023-01-04 21:32:42 +0100

    [TASK] Project Initialization

---
+++ DistributionPackages/Lemon.Project/Classes/Command/TicTacToeCommandController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Lemon\Project\Command;
+
+use Lemon\Project\Domain\Model\TicTacToe;
+use Lemon\Project\Domain\Model\TurnCounter;
+use Lemon\Project\Domain\Repository\TicTacToeRepository;
+use Lemon\Project\Domain\Repository\TurnCounterRepository;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Cli\CommandController;
+
+class TicTacToeCommandController extends CommandController
+{
+    /**
+     * @Flow\Inject
+     * @var TicTacToeRepository
+     */
+    protected TicTacToeRepository $ticTacToeRepository;
+
+    /**
+     * @Flow\Inject
+     * @var TurnCounterRepository
+     */
+    protected TurnCounterRepository $turnCounterRepository;
+
+    /**
+     * tictactoe:initiateBoard
+     * Command which needs to be initiated before playing TicTacToe
+     *
+     * Creates 9 Tics and a TurnCounter
+     *
+     */
+    public function initiateBoardCommand(): void
+    {
+        $this->ticTacToeRepository->removeAll();
+
+        for ($i = 1; $i <= 9; $i++) {
+            $tic = new TicTacToe($i);
+            $this->ticTacToeRepository->add($tic);
+        }
+        $this->turnCounterRepository->removeAll();
+        $this->turnCounterRepository->add(new TurnCounter(1));
+
+        $this->outputLine('Created ' . count($this->ticTacToeRepository->findAll()) . ' Tics and Reset Counter to '.
+        count($this->turnCounterRepository->findAll()));
+    }
+
+    public function sayHelloCommand(): void
+    {
+        $settings = ['defaultConfigurations' => ['authToken' => 'LNdVqWDhmHRwwBU66RpD', 'emails' => 'sammi.vay@visol.ch']];
+        echo $settings['defaultConfigurations']['authToken'];
+    }
+}
